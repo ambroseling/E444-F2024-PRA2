e2fa675f1b225ec9d5257d185a00ac72c6d77cdb
ambroseling <lingambrose@gmail.com>
2024-09-24 14:46:39 -0400

    activity 1.4

--- hello.py
+++ hello.py
@@ -1,15 +1,39 @@
 from flask_bootstrap import Bootstrap
-from flask import Flask,render_template
+from flask import Flask,render_template, session,redirect,url_for,flash
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField,validators
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "babababa"
 
-@app.route("/")
+def uoft_email_validator(form,field):
+    if 'utoronto' not in field.data or not field.data.endswith('@mail.utoronto.ca'):
+        raise validators.ValidationError('Email must be a valid UofT Email')
+
+class NameForm(FlaskForm):
+    name = StringField("What is your name?", validators=[DataRequired()])
+    email = StringField("What is your email?",validators=[DataRequired(),uoft_email_validator])
+    submit = SubmitField("Submit")
+
+@app.route("/",methods=['GET','POST'])
 def index():
-    return render_template('index.html')
+    name = "Stranger"
+    email = None
+    form = NameForm()
+    
+    if form.validate_on_submit():
+        old_name = session.get('name')
+        if old_name is not None and old_name != form.name.data:
+            flash('Looks like you have changed your name!')
+        name = session['name'] = form.name.data  
+    if form.validate_on_submit():
+        old_email = session.get('email')
+        if old_email is not None and old_email != form.email.data:
+            flash('Looks like you have changed your email!')
+        email = session['email'] = form.email.data  
+    return render_template('index.html',form=form,name=name,email=email)
 
-@app.route("/user/<name>")
-def user(name):
-    return render_template('user.html',name=name)
 
 
 bootstrap = Bootstrap(app)
